5b7b7f3e22f1091e9568140815d80a65b69c0655
wlnie <wlnie@mail.nfsq.com.cn>
2019-10-09 14:48:47 +0800

    1. 策略模式注释添加

--- study-witness-biz/src/main/java/com/goule666/study/witness/design/pattern/strategy/Customer.java
+++ study-witness-biz/src/main/java/com/goule666/study/witness/design/pattern/strategy/Customer.java
@@ -6,8 +6,6 @@ import java.math.BigDecimal;
  * @author wlnie
  * @date 2019/9/8 20:52
  * @description 策略模式的实现 结合简单工厂模式、单例模式
- * 具体例子描述：顾客购买商品，根据总消费金额分为三个等级，普通、VIP、SUPER_VIP 三种 其中VIP打八折、SUPER_VIP打5折
- * 消费总金额：1. 默认：普通、2. 5000：VIP、3. 10000：SUPER_VIP
  **/
 public class Customer {
     /**
--- study-witness-biz/src/main/java/com/goule666/study/witness/design/pattern/strategy/Test.java
+++ study-witness-biz/src/main/java/com/goule666/study/witness/design/pattern/strategy/Test.java
@@ -5,7 +5,17 @@ import java.math.BigDecimal;
 /**
  * @author wlnie
  * @date 2019/9/8 21:39
- * @description 测试类
+ * @description 策略模式
+ * 定义：策略模式定义了一系列的算法，并将每个算法封装起来，而且使它们还可以相互替换。策略模式让算法独立于使用它的客户而独立变化。
+ * 自我理解：一个抽象接口定义具体行为，N个算法类实现这个接口，客户端依赖抽象接口的行为，不依赖具体的算法
+ *
+ * 当前案例场景：
+ * 顾客购买商品，分为以下两种活动：
+ * 1. 根据总消费金额分为三个等级，普通、VIP、SUPER_VIP 三种 其中VIP打八折、SUPER_VIP打5折
+ * 2. 满减活动
+ * 其中满减活动优先级大于会员等级活动。
+ * 本例子采用：策略模式，单例模式，简单工厂模式，代理模式
+ *
  **/
 public class Test {
     public static void main(String[] args) {
