8efd58091b9f406050b679491cf7cb808fdf20f9
wlnie <wlnie@mail.nfsq.com.cn>
2019-10-15 23:35:52 +0800

    1. 修改注释

--- study-witness-biz/src/main/java/com/goule666/study/witness/design/pattern/strategy/memberPrice/CreateCalPriceFactory.java
+++ study-witness-biz/src/main/java/com/goule666/study/witness/design/pattern/strategy/memberPrice/CreateCalPriceFactory.java
@@ -5,6 +5,7 @@ import org.springframework.util.CollectionUtils;
 import java.io.File;
 import java.io.FileFilter;
 import java.lang.annotation.Annotation;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
@@ -61,13 +62,14 @@ public class CreateCalPriceFactory {
                     }
                 }
             }
+            //返回代理类
             return CalPriceProxy.getProxy(sortedMap);
         }
         return null;
     }
 
     /**
-     * 根据class得到注解是TotalValidRegion的类
+     * 根据class得到注解是TotalValidRegion和OnceValidRegion的类
      *
      * @param clazz class
      * @return TotalValidRegion
@@ -90,6 +92,9 @@ public class CreateCalPriceFactory {
         init();
     }
 
+    /**
+     * 加载所有的策略
+     */
     private void init() {
         try {
             clazzList = new ArrayList<>(10);
@@ -100,7 +105,9 @@ public class CreateCalPriceFactory {
             Class<CalPrice> calPriceClass = CalPrice.class;
             Class<? extends CalPrice> clazz;
             for (File f : files) {
+                //根据得到的file文件的名称+包名，加载class
                 clazz = (Class<? extends CalPrice>) classLoader.loadClass((PACKAGE_NAME + "." + f.getName()).replace(".class", ""));
+                //必须是CalPrice的子类，并且排除CalPrice本身
                 if (calPriceClass.isAssignableFrom(clazz) && clazz != calPriceClass) {
                     clazzList.add(clazz);
                 }
@@ -113,12 +120,13 @@ public class CreateCalPriceFactory {
     }
 
     /**
-     * 得到所有存放策略的class的文件名称
+     * 得到和指定接口类所在的同一个包下的所有以.class结尾的file文件
      *
      * @return File[]
      */
     private File[] getResources() {
         try {
+            //1. 调用classLoader.getResource根据给定接口的包名路径获取统一资源标识符，并得到file
             File file = new File(classLoader.getResource(PACKAGE_NAME.replace(".", "/")).toURI());
             return file.listFiles(new FileFilter() {
                 @Override
@@ -139,6 +147,11 @@ public class CreateCalPriceFactory {
         private static CreateCalPriceFactory createCalPriceFactory = new CreateCalPriceFactory();
     }
 
+    /**
+     * 采用单例模式 防止被实例化多次
+     *
+     * @return CreateCalPriceFactory
+     */
     public static CreateCalPriceFactory getInstance() {
         return GetSingleTon.createCalPriceFactory;
     }
