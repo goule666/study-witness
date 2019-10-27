ad5ded063c96c1a3bee025d19518e985ea6be4dd
wlnie <wlnie@mail.nfsq.com.cn>
2019-10-28 00:03:46 +0800

    拷贝 1. 浅拷贝记录下 后面需要多看下拷贝方面的工具类

--- study-witness-biz/src/main/java/com/goule666/study/witness/java/Test.java
+++ study-witness-biz/src/main/java/com/goule666/study/witness/java/Test.java
@@ -1,9 +1,32 @@
 package com.goule666.study.witness.java;
 
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author wlnie
  * @date 2019/10/10 11:48
  * @description
  **/
 public class Test {
+
+    public Map<String, String> map1 = new HashMap<>(16);
+
+    public Map<String, String> map2 ;
+
+    {
+        map1.put("1", "1");
+        //如果这里是new HashMap<>(map1)的话 map2就不会随着map1的变化而变化了
+        map2 = Collections.unmodifiableMap(map1);
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.map1.put("2", "2");
+        System.out.println(test);
+    }
+
+
 }
