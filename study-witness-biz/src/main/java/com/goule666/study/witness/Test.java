aca50f67a278bb9c09b05981a38d51416c6afdca
wlnie <wlnie@mail.nfsq.com.cn>
2019-12-28 20:43:09 +0800

    [Minor] 测试openjdk 8 使用的垃圾回收器

--- study-witness-biz/src/main/java/com/goule666/study/witness/Test.java
+++ study-witness-biz/src/main/java/com/goule666/study/witness/Test.java
@@ -1,5 +1,8 @@
 package com.goule666.study.witness;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author wlnie
  * @date 2019/10/15 13:37
@@ -7,4 +10,16 @@ package com.goule666.study.witness;
  **/
 public class Test {
 
+    private List<Integer> list = new ArrayList<>();
+
+    private void test() {
+        for (int i = 0; i < 100000000; i++) {
+            list.add(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.test();
+    }
 }
